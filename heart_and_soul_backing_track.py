fdc644688ff3053cebcbc6e99b01e22b30a1068f
reesporte <reesedporter@gmail.com>
2021-12-31 15:52:55 -0600

    more notes!!!

--- heart_and_soul_backing_track.py
+++ heart_and_soul_backing_track.py
@@ -1,39 +1,66 @@
 from jc3000 import Sequence
+from dataclasses import dataclass
+
+
+@dataclass
+class Note:
+    note: str = ""
+    octave: int = 0
 
 
 def play_chord(s, chord):
-    note = chord[0]
+    n = chord[0]
     duration = 0.5
-    octave = 0 if chord[0] != "f" and chord[0] != "g" else -1
 
-    s.add_note(note, duration=duration, octave=octave, voice=0)
-    s.add_note(note, duration=duration, octave=octave, voice=1, silent=True)
-    s.add_note(note, duration=duration, octave=octave, voice=2, silent=True)
+    s.add_note(n.note, duration=duration, octave=n.octave, voice=0)
+    for i in range(1, len(chord)):
+        s.add_note(n.note, duration=duration, voice=i, silent=True)
 
     duration = 0.25
-
-    s.add_note(note, duration=duration, octave=octave, voice=0)
-    s.add_note(note, duration=duration, octave=octave, voice=1, silent=True)
-    s.add_note(note, duration=duration, octave=octave, voice=2, silent=True)
-
-    for voice, note in enumerate(chord):
-        s.add_note(
-            note, duration=0.5, octave=1 if chord[0] != "f" and chord[0] != "g" else 0, voice=voice
-        )
+    s.add_note(n.note, duration=duration, octave=n.octave, voice=0)
+    for i in range(1, len(chord)):
+        s.add_note(n.note, duration=duration, voice=i, silent=True)
 
     for voice, note in enumerate(chord):
-        s.add_note(
-            note, duration=0.25, octave=1 if chord[0] != "f" and chord[0] != "g" else 0, voice=voice
-        )
+        if note.note != "":
+            s.add_note(
+                note.note,
+                duration=0.5,
+                octave=note.octave,
+                voice=voice,
+            )
+        else:
+            s.add_note(
+                "a",
+                duration=0.5,
+                voice=voice,
+                silent=True,
+            )
+
+    for voice, note in enumerate(chord):
+        if note.note != "":
+            s.add_note(
+                note.note,
+                duration=0.25,
+                octave=note.octave,
+                voice=voice,
+            )
+        else:
+            s.add_note(
+                "a",
+                duration=0.25,
+                voice=voice,
+                silent=True,
+            )
 
 
 def main():
-    s = Sequence(voices=3)
+    s = Sequence(voices=6)
 
-    c = ["c", "e", "g"]
-    f = ["f", "a", "c"]
-    a_min = ["a", "c", "e"]
-    g = ["g", "b", "d"]
+    c = [Note("c"), Note("e"), Note("g", -1), Note("c", 1), Note("e"), Note()]
+    a_min = [Note("a"), Note("e"), Note("g", 1), Note("c", 1), Note("e", 1), Note()]
+    f = [Note("f", -1), Note("c"), Note("f"), Note("a", 1), Note("c", 1), Note("e", 1)]
+    g = [Note("g", -1), Note("b"), Note("d"), Note("g", 1), Note("b", 1), Note("f", 1)]
     chords = [c, a_min, f, g] * 60
 
     for chord in chords:
